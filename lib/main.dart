0f9440ab58248df3000d686e53b504a6cb6a470e
shimizu-saffle <shimizu.saffle@gmail.com>
2022-08-01 23:01:04 +0900

    ShimmerLoading

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer_loading_sample/circle_list_item.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: CardListItem(isLoading: true),
+      ),
     );
   }
 }
